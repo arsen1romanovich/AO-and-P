841e56f01162b5d413651f456919044ee5bcecf1
arsen1romanovich <arsenijsozinov8@gmail.com>
2026-04-08 10:32:17 +0300

    Add files via upload

---
+++ 2 задание.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int x;
+    cout << "введите баллы (0-100): ";
+    cin >> x;
+
+    if (x < 0 || x > 100) return cout << "Ошибка" << endl, 0;
+
+    if (x >= 90) cout << "отлично";
+    else if (x >= 70) cout << "хорошо";
+    else cout << (x >= 50 ? "удовлетворительно" : "неудовлетворительно");
+    cout << endl;
+
+    return 0;
+}
